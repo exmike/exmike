f87724624289ebefe1de83050dbef5c361d36f5e
exmike <36734883+exmike@users.noreply.github.com>
2018-04-29 14:48:45 +0300

    Delete Банк.py

--- Банк.py
+++
@@ -1,9 +0,0 @@
-import account
-def main():
-    rate = int(input("Введите ставку: "))
-    cash = int(input("\nВведите сумму: "))
-    period = int(input("\nВведите период: "))
-    result = account.calculate_income(rate, cash, period)
-    print (result,rate,cash)
-if __name__ == "__main__":
-    main ()
